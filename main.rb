e6dc5cca4778df60c29aa3f4fef0c1350ba0f03b
root <gerric.chaplin@gmail.com>
2014-11-23 22:06:04 +0000

    Debmirror initial testing with status to MongoDB.

--- main.rb
+++ main.rb
@@ -11,13 +11,18 @@ doc = {"_id" => "1"}
 id = coll.insert(doc)
 h = Hash.new
 
-cmd = 'wget http://www.particleflux.co.uk'
+cmd = 'debmirror -a i386 --no-source -s main -h archive.ubuntu.com -d trusty -r /ubuntu --progress --ignore-release-gpg --method=http /tmp/UbuntuMirror'
 Open3.popen2e(cmd) do |stdin, stdout_err, wait_thr|
   while line = stdout_err.gets
     line = line.split(" ")
-    h["status"] = line[6]
+
+    unless line[1].nil?
+     line = line[1].to_s.gsub(/\]/,"")
+      puts line.inspect
+      h["status"] = line
 	puts h
 	coll.update({"_id" => "1"}, h)
+    end
   end
 
   exit_status = wait_thr.value
